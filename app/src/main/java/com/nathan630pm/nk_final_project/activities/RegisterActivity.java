3b2aa0e53965f2e5eecb00d43606ee618d998c2e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-13 14:42:27 -0500

    Fixed google-services.json, added login & register pages

---
+++ app/src/main/java/com/nathan630pm/nk_final_project/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.nathan630pm.nk_final_project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nathan630pm.nk_final_project.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
